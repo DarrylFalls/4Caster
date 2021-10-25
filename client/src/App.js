77307be88256cafa80996f4c17e2c80e23f7fa33
Darryl Falls <darryl.a.falls@gmial.com>
2021-10-25 14:41:32 -0400

    added components

--- client/src/App.js
+++ client/src/App.js
@@ -6,19 +6,32 @@ function App() {
   const [user, setUser] = useState('')
   const [favorites, setFavorites] = useState([])
   const [currentForcast, setCurrentForcast] = ('')
+  const [location, setLocation] = useState('')
+  const [locationData, setLocationData] = useState('')
   // const []
 
   //weatherAPIkey = 5ae4ab16cf1646c9910231906212410
 
 
-  const getWeather = async () => {
-    const res = await axios.get(`http://api.weatherapi.com/v1/forecast.json?key=5ae4ab16cf1646c9910231906212410&q=London&days=7&aqi=yes&alerts=yes`)
+  const getLocation = async () => {
+    const res = await axios.get(`http://www.mapquestapi.com/geocoding/v1/address?key=bTdBubAIGCp23LC0DL0nfCNW3R4HzIQj&location=${location}`)
     console.log(res.data)
   }
+
+  const getWeather = async () => {
+    const res = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=50&lon=50&units=imperial&exclude=minutely&appid=ee5d53a1026a07610d0cd0ca65f2c148`)
+    console.log(res.data)
+  }
+
+
+  // const getWeather = async () => {
+  //   const res = await axios.get(`http://api.weatherapi.com/v1/forecast.json?key=5ae4ab16cf1646c9910231906212410&q=London&days=7&aqi=yes&alerts=yes`)
+  //   console.log(res.data)
+  // }
   const getUserInfo = async () => {
     const res = await axios.get('https://api.airtable.com/v0/app4ZMuiUaRsyIY94/Table%201?api_key=key3kKNmypHQOUSxM')
     setUser(res.data.records)
-    console.log(res.data.records[0].fields.favorites)
+    console.log(res.data.records)
   }
 
   useEffect(() => (
