54aa3a82b63a0f4dbf100ffdec57caca72b9d4c4
Darryl Falls <darryl.a.falls@gmial.com>
2021-10-28 22:48:52 -0400

    building daily forecast

--- client/src/components/DailyForcast.js
+++ client/src/components/DailyForcast.js
@@ -1,11 +1,24 @@
+import './DailyForcast.css'
 
+const DailyForcast = ({ weatherData }) => {
+  // const getDay = (dt) => {
+  //   const a = new Date(dt * 1000);
+  //   const months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  //   const year = a.getFullYear();
+  //   const month = months[a.getMonth()];
+  //   const date = a.getDate();
+  //   console.log(month)
+  //   return(`${date}/${month}/${year}`)
+  // }
 
-const DailyForcast = ({weatherData}) => {
+  if (weatherData) {
+    console.log(weatherData.daily)
+  }
   return (
-    <div>
-      {weatherData ? weatherData.daily.map((day) => (
-        <div className='day'>
-
+    <div className='forecast'>
+      {weatherData ? weatherData.daily.map((day, idx) => (
+        <div className='day' key={idx}>
+          <img src={`http://openweathermap.org/img/wn/${day.weather[0].icon}.png`} className='small-weather-icon' />
         </div>
       )) : null}
     </div>
